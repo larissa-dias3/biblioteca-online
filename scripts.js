bd864f4cd620c104821544aeb059ed7ea56117cd
Larissa Dias <154644799+larissa-dias3@users.noreply.github.com>
2025-05-04 19:22:55 -0300

    criando layout

---
+++ scripts.js
@@ -0,0 +1,40 @@
+const listaLivros = document.querySelector('.lista-livros');
+const destaque = document.querySelector('.livros-destaques');
+
+fetch('livros.json')
+    .then((response) => response.json())
+    .then((livros) => {
+        livros.forEach(livro => {
+            const livroItem = document.createElement('li');
+            livroItem.classList.add('livro');
+            livroItem.innerHTML = `
+                    <h2>${livro.titulo}</h2>                    
+                    <img src="${livro.imagem}" alt="${livro.alt}" class="livro">
+                    <p>${livro.autor}</p>
+                `;
+            listaLivros.appendChild(livroItem);
+        }); //forEach
+
+        const livrosEmDestaque = [
+            livros[livros.length - 1],
+            livros[livros.length - 2]
+        ];// pega o ultimo e penultimo livro do array
+        livrosEmDestaque.forEach((livro) => {
+            const blocoDestaque = document.createElement('div');
+            blocoDestaque.classList.add('livro-destaque');
+            blocoDestaque.innerHTML = `
+                <h2>${livro.titulo}</h2>
+                <img src="${livro.imagem}" alt="${livro.alt}" class="livro">
+                <p>${livro.autor}</p>
+            `;
+            destaque.appendChild(blocoDestaque);
+        });
+
+    })
+    
+    .catch(error => {
+        console.error('Erro ao carregar os livros:', error);
+    });
+
+
+
